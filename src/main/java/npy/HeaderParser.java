4e2cf718c8ceafade680221449df69cc6f0861d4
msrocka <michael.srocka@gmail.com>
2021-05-06 15:23:24 +0200

    maybe we should have a real lexer

--- src/main/java/npy/HeaderParser.java
+++ src/main/java/npy/HeaderParser.java
@@ -1,77 +1,136 @@
 package npy;
 
+import java.util.Objects;
+
 class HeaderParser {
 
   private final char EOF = Character.MIN_VALUE;
 
   private final String input;
-  private int pos;
+  private int pos = -1;
 
   private HeaderParser(String input) {
-    this.input = input;
+    this.input = Objects.requireNonNull(input);
   }
 
   private void parse() {
-    forceMoveTo('{');
+
+    char start = nextNonEmpty();
+    if (start == EOF)
+      throw HeaderFormatException.isEmpty(input);
+    if (start != '{')
+      throw HeaderFormatException.unexpectedCharAt(input, pos, start);
+
+    boolean fortranOrder = false;
+    int[] shape = null;
 
     // read the key value pairs
     while (true) {
 
-      // read the next key
-      char c = next();
+      // move to the next key
+      char c = nextNonEmpty();
       if (c == '}')
         break;
-      if (c != '\'' && c != '"')
-        throw
-
       if (c == EOF)
-        throw new IllegalArgumentException(
-          "invalid header: unexpected end at pos=" + pos);
+        throwEof();
+      if (!isQuote(c))
+        throw HeaderFormatException.unexpectedCharAt(input, pos, c);
+
+      // read key and colon
+      var key = eatString(c);
+      char colon = nextNonEmpty();
+      if (colon == EOF)
+        throwEof();
+      if (colon != ':')
+        throw HeaderFormatException.unexpectedCharAt(input, pos, colon);
+
+      // move to the value
+      char valStart = nextNonEmpty();
+      if (valStart == EOF)
+        throwEof();
+
+      switch (key) {
+        case "fortran_order":
+          fortranOrder = eatBoolean(valStart);
+          break;
+        case "shape":
+          //shape = eatShape(valStart);
+          break;
+      }
 
 
-
-      char cc = next();
+      char cc = nextNonEmpty();
       if (cc == '}')
         break;
       if (cc == ',')
         continue;
       if (cc == EOF)
-        throw new IllegalArgumentException(
-          "invalid header: unexpeced end of header at pos=" + pos);
-      throw new IllegalArgumentException(
-
-      )
+        throwEof();
 
     }
   }
 
   /**
-   * Moves the cursor to the next non-whitespace character.
+   * Moves the cursor to the position of the next non-whitespace character and
+   * returns the character at that position.
    */
-  private char next() {
-
+  private char nextNonEmpty() {
+    while (true) {
+      char nextChar = next();
+      if (Character.isWhitespace(nextChar))
+        continue;
+      return nextChar;
+    }
   }
 
   /**
-   * Moves the cursor to the given character skipping all whitespaces. Throws an
-   * error if another non-whitespace character was found.
+   * Moves the cursor to the position of the next character and returns the
+   * character at that position. Returns EOF if there is no character anymore.
    */
-  private void forceMoveTo(char c) {
+  private char next() {
+    int nextPos = pos + 1;
+    if (nextPos >= input.length())
+      return EOF;
+    pos = nextPos;
+    return input.charAt(nextPos);
+  }
+
+  private String eatString(char firstQuote) {
+    var buffer = new StringBuilder();
     while (true) {
-      var next = peek();
-      if (next == EOF)
-        throw new IllegalArgumentException(
-          "invalid header: expected to find '"
-          + c + "' at pos=" + pos + " in " + input);
+      char c = next();
+      if (c == firstQuote)
+        break;
+      if (c == EOF)
+        throwEof();
+      buffer.append(c);
+    }
+    return buffer.toString();
+  }
+
+  private boolean eatBoolean(char firstChar) {
+    var buffer = new StringBuilder();
+    buffer.append(firstChar);
+    while (true) {
+      char c = peek();
+      if (Character.isWhitespace(c) || c == ',')
+        break;
+      if (c == EOF)
+        throwEof();
       pos++;
-      if (next == c)
-        return;
-      if (Character.isWhitespace(next))
-        continue;
-      throw new IllegalArgumentException(
-        "invalid header: expected to find '"
-        + c + "' but found " + next + "at pos="
-        + pos + " in " + input);
+      buffer.append(c);
+    }
+    var s = buffer.toString().toLowerCase();
+    switch (s) {
+      case "true":
+      case "1":
+        return true;
+      case "false":
+      case "0":
+        return false;
+      default:
+        throw new HeaderFormatException(
+          input, "invalid Boolean", pos - s.length());
     }
   }
 
@@ -82,5 +141,12 @@ class HeaderParser {
       : input.charAt(nextPos);
   }
 
+  private boolean isQuote(char c) {
+    return c == '"' || c == '\'';
+  }
+
+  private void throwEof() {
+    throw HeaderFormatException.unexpectedEofAt(input, pos);
+  }
 
 }
