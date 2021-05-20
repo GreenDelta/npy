12023745a3d8014faa8ad7654dabc3da104b7d1f
msrocka <michael.srocka@gmail.com>
2021-05-20 18:33:04 +0200

    make HeaderDictionary immutable

--- src/main/java/org/openlca/npy/dict/HeaderDictionary.java
+++ src/main/java/org/openlca/npy/dict/HeaderDictionary.java
@@ -1,8 +1,11 @@
 package org.openlca.npy.dict;
 
 import java.nio.ByteOrder;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import org.openlca.npy.DataType;
 import org.openlca.npy.UnsupportedFormatException;
@@ -11,23 +14,28 @@ import org.openlca.npy.UnsupportedFormatException;
  * Contains the values of the dictionary that is stored in the header of an NPY
  * file.
  */
-public class Dict {
+public class HeaderDictionary {
 
   private final DataType dataType;
   private final ByteOrder byteOrder;
   private final boolean fortranOrder;
   private final int[] shape;
+  private final Map<String, String> properties;
 
-  private final Map<String, String> properties = new HashMap<>();
+  public HeaderDictionary(Builder builder) {
+    this.dataType = builder.dataType;
+    this.shape = Arrays.copyOf(builder.dimensions, builder.dimensions.length);
+    this.byteOrder = builder.byteOrder == null
+      ? ByteOrder.nativeOrder()
+      : builder.byteOrder;
+    this.fortranOrder = builder.fortranOrder;
+    this.properties = builder.properties != null
+      ? builder.properties
+      : Collections.emptyMap();
+  }
 
-  public Dict(DataType dataType,
-              ByteOrder byteOrder,
-              boolean fortranOrder,
-              int[] shape) {
-    this.dataType = dataType;
-    this.byteOrder = byteOrder;
-    this.fortranOrder = fortranOrder;
-    this.shape = shape;
+  public static Builder of(DataType dataType, int[] dimensions) {
+    return new Builder(dataType, dimensions);
   }
 
   public DataType dataType() {
@@ -42,11 +50,33 @@ public class Dict {
     return fortranOrder;
   }
 
-  public int[] shape() {
-    return shape;
+  /**
+   * Returns the number of dimensions of the array..
+   */
+  public int dimensions() {
+    return shape.length;
   }
 
-  public static Dict parse(String s) {
+  /**
+   * Returns the size if the ith dimension of the array.
+   *
+   * @param i the 0-based dimension for which the size is requested.
+   * @return the size of the requested dimension
+   * @throws IndexOutOfBoundsException if {@code i < 0 || i >= dimensions()}
+   */
+  public int sizeOfDimension(int i) {
+    if (i < 0 || i >= shape.length)
+      throw new IndexOutOfBoundsException(i);
+    return shape[i];
+  }
+
+  public Map<String, String> otherProperties() {
+    return properties.isEmpty()
+      ? Collections.emptyMap()
+      : Collections.unmodifiableMap(properties);
+  }
+
+  public static HeaderDictionary parse(String s) {
     var value = Parser.parse(s);
     if (value.isError())
       throw new UnsupportedFormatException(
@@ -124,4 +154,37 @@ public class Dict {
       "unsupported data type: '" + symbol + "'");
   }
 
+  public static class Builder {
+
+    private final DataType dataType;
+    private final int[] dimensions;
+    private ByteOrder byteOrder;
+    private boolean fortranOrder;
+    private Map<String, String> properties;
+
+    private Builder(DataType dataType, int[] dimensions) {
+      this.dataType = Objects.requireNonNull(dataType);
+      this.dimensions = Objects.requireNonNull(dimensions);
+      if (dimensions.length == 0)
+        throw new IllegalArgumentException("no dimensions");
+    }
+
+    public Builder withByteOrder(ByteOrder byteOrder) {
+      this.byteOrder = byteOrder;
+      return this;
+    }
+
+    public Builder withFortranOrder(boolean b) {
+      this.fortranOrder = b;
+      return this;
+    }
+
+    public Builder withOtherProperty(String key, String value) {
+      if (properties == null) {
+        properties = new HashMap<>();
+      }
+      properties.put(key, value);
+      return this;
+    }
+  }
 }
