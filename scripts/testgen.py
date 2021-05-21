b5f7079813db79529947f04a104d1946d65f20b3
msrocka <michael.srocka@gmail.com>
2021-05-21 16:35:59 +0200

    move testgen script

--- scripts/testgen.py
+++ scripts/testgen.py
@@ -5,7 +5,7 @@ import numpy as np
 import scipy.sparse
 
 
-TESTDIR = "../../../target/testdata"
+TESTDIR = "../target/testdata"
 
 
 def write_u8bin():
@@ -39,28 +39,39 @@ def write_sparse_npz():
 
 def generate_npy_files():
 
-    types_i = [
-        ('i', 'b'),
+    types = [
+        ('i1', 'b'),
         ('i2', 'i2'),
-        ('u', 'B'),
+        ('i4', 'i4'),
+        ('i8', 'i8'),
+        ('u1', 'B'),
         ('u2', 'u2'),
+        ('u4', 'u4'),
+        ('u8', 'u8'),
+        ('f2', 'f2'),
+        ('f4', 'f4'),
+        ('f8', 'f8'),
     ]
 
-    data_i = [[1, 2, 3],
-         [4, 5, 6]]
-    
-    np.save(TESTDIR + '/i_le_f.npy', data_i)
+    data = [
+        [1, 2, 3],
+        [4, 5, 6],
+    ]
+
+    for (t1, t2) in types:
+        for order in ('C', 'F'):
+            for endianness in ('le', 'be'):
+                file_name = '/%s_%s_%s.npy' % (t1, endianness, order.lower())
+                prefix = '<' if endianness == 'le' else '>'
+                np.save(TESTDIR + file_name,
+                        np.array(int_data, order=order, dtype=prefix+t2))
 
     """
-    np.save(TESTDIR + '/i8_le_f.npy', np.array(
-        data_i,
-        order='F', dtype='<f8'))
-    """
-
     np.save(TESTDIR + '/f8_le_f.npy', np.array(
         [[1.0, 2.0, 3.0],
          [4.0, 5.0, 6.0]],
         order='F', dtype='<f8'))
+    """
 
 
 if __name__ == "__main__":
