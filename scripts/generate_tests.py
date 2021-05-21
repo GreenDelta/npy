a6ef48e805dea96b6992e40337a2c5da3e1ceb1e
msrocka <michael.srocka@gmail.com>
2021-05-21 20:39:00 +0200

    generate also boolean matrices and a combined npz file

---
+++ scripts/generate_tests.py
@@ -0,0 +1,56 @@
+import os
+import numpy as np
+import zipfile
+
+
+TESTDIR = "../target/testdata"
+
+
+def main():
+
+    # initialize the test folder
+    if not os.path.exists(TESTDIR):
+        os.makedirs(TESTDIR)
+    for f in os.listdir(TESTDIR):
+        os.remove(TESTDIR + "/" + f)
+
+    types = [
+        ('bool', '?'),
+        ('i1', 'b'),
+        ('i2', 'i2'),
+        ('i4', 'i4'),
+        ('i8', 'i8'),
+        ('u1', 'B'),
+        ('u2', 'u2'),
+        ('u4', 'u4'),
+        ('u8', 'u8'),
+        ('f2', 'f2'),
+        ('f4', 'f4'),
+        ('f8', 'f8'),
+    ]
+
+    data = [
+        [0, 1, 2],
+        [3, 4, 5],
+    ]
+
+    # generate the npy files for all combinations of storage order and
+    # endianness for all data types
+    for (t1, t2) in types:
+        for order in ('C', 'F'):
+            for endianness in ('le', 'be'):
+                file_name = '/%s_%s_%s.npy' % (t1, endianness, order.lower())
+                prefix = '<' if endianness == 'le' else '>'
+                np.save(TESTDIR + file_name,
+                        np.array(data, order=order, dtype=prefix+t2))
+
+    # create the npz file with all generated npy files
+    with zipfile.ZipFile(TESTDIR + "/all.npz", 'w') as z:
+        for f in os.listdir(TESTDIR):
+            if not f.endswith('.npy'):
+                continue
+            z.write(TESTDIR + '/' + f, f)
+
+
+if __name__ == "__main__":
+    main()
