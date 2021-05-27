19a6884a99ff3978b97f1b2d93043f8db7db0c3f
msrocka <michael.srocka@gmail.com>
2021-05-27 18:16:28 +0200

    all double tests are good

--- src/test/java/org/openlca/npy/NpyTest.java
+++ src/test/java/org/openlca/npy/NpyTest.java
@@ -18,13 +18,38 @@ public class NpyTest {
       assertTrue(Array2D.isValid(array));
       assertEquals(2, Array2D.rowCountOf(array));
       assertEquals(3, Array2D.columnCountOf(array));
-
-      var doubles = array.asDoubleArray();
-      assertArrayEquals(
-        new double[]{0, 1, 2}, Array2D.getRow(doubles, 0), 1e-9);
-      assertArrayEquals(
-        new double[]{3, 4, 5}, Array2D.getRow(doubles, 1), 1e-9);
+      checkDoubles(array.asDoubleArray());
     });
 
+  }
+
+  private void checkDoubles(NpyDoubleArray array) {
+
+    // check each element
+    var expected = new double[][]{
+      new double[]{0, 1, 2},
+      new double[]{3, 4, 5},
+    };
+    for (int row = 0; row < 2; row++) {
+      for (int col = 0; col < 3; col++) {
+        assertEquals(expected[row][col], Array2D.get(array, row, col), 1e-16);
+      }
+    }
+
+    // check by rows
+    assertArrayEquals(expected[0], Array2D.getRow(array, 0), 1e-16);
+    assertArrayEquals(expected[1], Array2D.getRow(array, 1), 1e-16);
+
+    // check by columns
+    assertArrayEquals(new double[]{0, 3}, Array2D.getColumn(array, 0), 1e-16);
+    assertArrayEquals(new double[]{1, 4}, Array2D.getColumn(array, 1), 1e-16);
+    assertArrayEquals(new double[]{2, 5}, Array2D.getColumn(array, 2), 1e-16);
+
+    // check by storage order
+    if (array.hasFortranOrder()) {
+      assertArrayEquals(new double[]{0, 3, 1, 4, 2, 5}, array.data, 1e-16);
+    } else {
+      assertArrayEquals(new double[]{0, 1, 2, 3, 4, 5}, array.data, 1e-16);
+    }
   }
 }
