b670432201dec8ed253db129b10901fb580f63a5
msrocka <michael.srocka@gmail.com>
2021-09-03 13:36:55 +0200

    implemented all order switches
    in Java 17 we would make NpyArray a sealed interface and
    only permit the array types in the `arrays` package for
    implementation ...

--- src/main/java/org/openlca/npy/arrays/OrderSwitch2d.java
+++ src/main/java/org/openlca/npy/arrays/OrderSwitch2d.java
@@ -1,5 +1,7 @@
 package org.openlca.npy.arrays;
 
+import java.math.BigInteger;
+
 class OrderSwitch2d<T extends NpyArray<?>> {
 
   private final T array;
@@ -18,10 +20,82 @@ class OrderSwitch2d<T extends NpyArray<?>> {
 
   @SuppressWarnings("unchecked")
   private T apply() {
-    if (array.isDoubleArray()) {
+    if (array.isBigIntegerArray())
+      return (T) switchBigInts((NpyBigIntArray) array);
+    if (array.isBooleanArray())
+      return (T) switchBooleans(array.asBooleanArray());
+    if (array.isByteArray())
+      return (T) switchBytes(array.asByteArray());
+    if (array.isCharArray())
+      return (T) switchChars(array.asCharArray());
+    if (array.isDoubleArray())
       return (T) switchDoubles(array.asDoubleArray());
-    }
-    return null;
+    if (array.isFloatArray())
+      return (T) switchFloats(array.asFloatArray());
+    if (array.isIntArray())
+      return (T) switchInts(array.asIntArray());
+    if (array.isLongArray())
+      return (T) switchLongs(array.asLongArray());
+    if (array.isShortArray())
+      return (T) switchShorts(array.asShortArray());
+
+    throw new IllegalArgumentException("unsupported array type: " + array);
+  }
+
+  private NpyBigIntArray switchBigInts(NpyBigIntArray a) {
+    var data = a.data;
+    var newData = new BigInteger[data.length];
+    iter((pos, newPos) -> {
+      var value = data[pos];
+      if (value == null)
+        return;
+      newData[newPos] = value; // BigInts are immutable; so this is fine
+    });
+    return a.hasColumnOrder()
+      ? NpyBigIntArray.rowOrderOf(newData, rows, cols)
+      : NpyBigIntArray.columnOrderOf(newData, rows, cols);
+  }
+
+  private NpyBooleanArray switchBooleans(NpyBooleanArray a) {
+    var data = a.data;
+    var newData = new boolean[data.length];
+    iter((pos, newPos) -> {
+      var value = data[pos];
+      if (!value)
+        return;
+      newData[newPos] = true;
+    });
+    return a.hasColumnOrder()
+      ? NpyBooleanArray.rowOrderOf(newData, rows, cols)
+      : NpyBooleanArray.columnOrderOf(newData, rows, cols);
+  }
+
+  private NpyByteArray switchBytes(NpyByteArray a) {
+    var data = a.data;
+    var newData = new byte[data.length];
+    iter((pos, newPos) -> {
+      var value = data[pos];
+      if (value == 0)
+        return;
+      newData[newPos] = value;
+    });
+    return a.hasColumnOrder()
+      ? NpyByteArray.rowOrderOf(newData, rows, cols)
+      : NpyByteArray.columnOrderOf(newData, rows, cols);
+  }
+
+  private NpyCharArray switchChars(NpyCharArray a) {
+    var data = a.data;
+    var newData = new char[data.length];
+    iter((pos, newPos) -> {
+      var value = data[pos];
+      if (value == 0)
+        return;
+      newData[newPos] = value;
+    });
+    return a.hasColumnOrder()
+      ? new NpyCharArray(new int[]{rows, cols}, newData, false)
+      : new NpyCharArray(new int[]{rows, cols}, newData, true);
   }
 
   private NpyDoubleArray switchDoubles(NpyDoubleArray a) {
@@ -36,6 +110,62 @@ class OrderSwitch2d<T extends NpyArray<?>> {
     return a.hasColumnOrder()
       ? NpyDoubleArray.rowOrderOf(newData, rows, cols)
       : NpyDoubleArray.columnOrderOf(newData, rows, cols);
+  }
+
+  private NpyFloatArray switchFloats(NpyFloatArray a) {
+    var data = a.data;
+    var newData = new float[data.length];
+    iter((pos, newPos) -> {
+      var value = data[pos];
+      if (value == 0)
+        return;
+      newData[newPos] = value;
+    });
+    return a.hasColumnOrder()
+      ? NpyFloatArray.rowOrderOf(newData, rows, cols)
+      : NpyFloatArray.columnOrderOf(newData, rows, cols);
+  }
+
+  private NpyIntArray switchInts(NpyIntArray a) {
+    var data = a.data;
+    var newData = new int[data.length];
+    iter((pos, newPos) -> {
+      var value = data[pos];
+      if (value == 0)
+        return;
+      newData[newPos] = value;
+    });
+    return a.hasColumnOrder()
+      ? NpyIntArray.rowOrderOf(newData, rows, cols)
+      : NpyIntArray.columnOrderOf(newData, rows, cols);
+  }
+
+  private NpyLongArray switchLongs(NpyLongArray a) {
+    var data = a.data;
+    var newData = new long[data.length];
+    iter((pos, newPos) -> {
+      var value = data[pos];
+      if (value == 0)
+        return;
+      newData[newPos] = value;
+    });
+    return a.hasColumnOrder()
+      ? NpyLongArray.rowOrderOf(newData, rows, cols)
+      : NpyLongArray.columnOrderOf(newData, rows, cols);
+  }
+
+  private NpyShortArray switchShorts(NpyShortArray a) {
+    var data = a.data;
+    var newData = new short[data.length];
+    iter((pos, newPos) -> {
+      var value = data[pos];
+      if (value == 0)
+        return;
+      newData[newPos] = value;
+    });
+    return a.hasColumnOrder()
+      ? NpyShortArray.rowOrderOf(newData, rows, cols)
+      : NpyShortArray.columnOrderOf(newData, rows, cols);
   }
 
   private void iter(IndexFn fn) {
--- src/test/java/org/openlca/npy/arrays/OrderSwitch2Test.java
+++ src/test/java/org/openlca/npy/arrays/OrderSwitch2Test.java
@@ -1,7 +1,167 @@
 package org.openlca.npy.arrays;
 
+import java.math.BigInteger;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class OrderSwitch2Test {
 
-  
+  @Test
+  public void testBigInts() {
+    var dataRows = new BigInteger[]{b(1), b(2), b(3), b(4), b(5), b(6)};
+    var dataCols = new BigInteger[]{b(1), b(4), b(2), b(5), b(3), b(6)};
+
+    var rowArray = NpyBigIntArray.rowOrderOf(dataRows, 2, 3);
+    assertTrue(rowArray.hasRowOrder());
+
+    var colArray = Array2d.switchOrder(rowArray);
+    assertTrue(colArray.hasColumnOrder());
+    assertArrayEquals(dataCols, colArray.data());
+    assertArrayEquals(rowArray.shape(), colArray.shape());
+
+    var rowArray2 = Array2d.switchOrder(colArray);
+    assertTrue(rowArray2.hasRowOrder());
+    assertArrayEquals(dataRows, rowArray2.data());
+    assertArrayEquals(rowArray.shape(), rowArray2.shape());
+  }
+
+  private BigInteger b(long value) {
+    return BigInteger.valueOf(value);
+  }
+
+  @Test
+  public void testBooleans() {
+    var dataRows = new boolean[]{true, true, true, false, false, false};
+    var dataCols = new boolean[]{true, false, true, false, true, false};
+
+    var rowArray = NpyBooleanArray.rowOrderOf(dataRows, 2, 3);
+    assertTrue(rowArray.hasRowOrder());
+
+    var colArray = Array2d.switchOrder(rowArray);
+    assertTrue(colArray.hasColumnOrder());
+    assertArrayEquals(dataCols, colArray.data());
+    assertArrayEquals(rowArray.shape(), colArray.shape());
+
+    var rowArray2 = Array2d.switchOrder(colArray);
+    assertTrue(rowArray2.hasRowOrder());
+    assertArrayEquals(dataRows, rowArray2.data());
+    assertArrayEquals(rowArray.shape(), rowArray2.shape());
+  }
+
+  @Test
+  public void testBytes() {
+    var dataRows = new byte[]{1, 2, 3, 4, 5, 6};
+    var dataCols = new byte[]{1, 4, 2, 5, 3, 6};
+
+    var rowArray = NpyByteArray.rowOrderOf(dataRows, 2, 3);
+    assertTrue(rowArray.hasRowOrder());
+
+    var colArray = Array2d.switchOrder(rowArray);
+    assertTrue(colArray.hasColumnOrder());
+    assertArrayEquals(dataCols, colArray.data());
+    assertArrayEquals(rowArray.shape(), colArray.shape());
+
+    var rowArray2 = Array2d.switchOrder(colArray);
+    assertTrue(rowArray2.hasRowOrder());
+    assertArrayEquals(dataRows, rowArray2.data());
+    assertArrayEquals(rowArray.shape(), rowArray2.shape());
+  }
+
+  @Test
+  public void testDoubles() {
+    var dataRows = new double[]{1, 2, 3, 4, 5, 6};
+    var dataCols = new double[]{1, 4, 2, 5, 3, 6};
+
+    var rowArray = NpyDoubleArray.rowOrderOf(dataRows, 2, 3);
+    assertTrue(rowArray.hasRowOrder());
+
+    var colArray = Array2d.switchOrder(rowArray);
+    assertTrue(colArray.hasColumnOrder());
+    assertArrayEquals(dataCols, colArray.data(), 1e-10);
+    assertArrayEquals(rowArray.shape(), colArray.shape());
+
+    var rowArray2 = Array2d.switchOrder(colArray);
+    assertTrue(rowArray2.hasRowOrder());
+    assertArrayEquals(dataRows, rowArray2.data(), 1e-10);
+    assertArrayEquals(rowArray.shape(), rowArray2.shape());
+  }
+
+  @Test
+  public void testFloats() {
+    var dataRows = new float[]{1, 2, 3, 4, 5, 6};
+    var dataCols = new float[]{1, 4, 2, 5, 3, 6};
+
+    var rowArray = NpyFloatArray.rowOrderOf(dataRows, 2, 3);
+    assertTrue(rowArray.hasRowOrder());
+
+    var colArray = Array2d.switchOrder(rowArray);
+    assertTrue(colArray.hasColumnOrder());
+    assertArrayEquals(dataCols, colArray.data(), 1e-6f);
+    assertArrayEquals(rowArray.shape(), colArray.shape());
+
+    var rowArray2 = Array2d.switchOrder(colArray);
+    assertTrue(rowArray2.hasRowOrder());
+    assertArrayEquals(dataRows, rowArray2.data(), 1e-6f);
+    assertArrayEquals(rowArray.shape(), rowArray2.shape());
+  }
+
+  @Test
+  public void testInts() {
+    var dataRows = new int[]{1, 2, 3, 4, 5, 6};
+    var dataCols = new int[]{1, 4, 2, 5, 3, 6};
+
+    var rowArray = NpyIntArray.rowOrderOf(dataRows, 2, 3);
+    assertTrue(rowArray.hasRowOrder());
+
+    var colArray = Array2d.switchOrder(rowArray);
+    assertTrue(colArray.hasColumnOrder());
+    assertArrayEquals(dataCols, colArray.data());
+    assertArrayEquals(rowArray.shape(), colArray.shape());
+
+    var rowArray2 = Array2d.switchOrder(colArray);
+    assertTrue(rowArray2.hasRowOrder());
+    assertArrayEquals(dataRows, rowArray2.data());
+    assertArrayEquals(rowArray.shape(), rowArray2.shape());
+  }
+
+  @Test
+  public void testLongs() {
+    var dataRows = new long[]{1, 2, 3, 4, 5, 6};
+    var dataCols = new long[]{1, 4, 2, 5, 3, 6};
+
+    var rowArray = NpyLongArray.rowOrderOf(dataRows, 2, 3);
+    assertTrue(rowArray.hasRowOrder());
+
+    var colArray = Array2d.switchOrder(rowArray);
+    assertTrue(colArray.hasColumnOrder());
+    assertArrayEquals(dataCols, colArray.data());
+    assertArrayEquals(rowArray.shape(), colArray.shape());
+
+    var rowArray2 = Array2d.switchOrder(colArray);
+    assertTrue(rowArray2.hasRowOrder());
+    assertArrayEquals(dataRows, rowArray2.data());
+    assertArrayEquals(rowArray.shape(), rowArray2.shape());
+  }
+
+  @Test
+  public void testShorts() {
+    var dataRows = new short[]{1, 2, 3, 4, 5, 6};
+    var dataCols = new short[]{1, 4, 2, 5, 3, 6};
+
+    var rowArray = NpyShortArray.rowOrderOf(dataRows, 2, 3);
+    assertTrue(rowArray.hasRowOrder());
+
+    var colArray = Array2d.switchOrder(rowArray);
+    assertTrue(colArray.hasColumnOrder());
+    assertArrayEquals(dataCols, colArray.data());
+    assertArrayEquals(rowArray.shape(), colArray.shape());
+
+    var rowArray2 = Array2d.switchOrder(colArray);
+    assertTrue(rowArray2.hasRowOrder());
+    assertArrayEquals(dataRows, rowArray2.data());
+    assertArrayEquals(rowArray.shape(), rowArray2.shape());
+  }
 
 }
